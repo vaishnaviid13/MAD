7f27943db1a51cd825eaab90542406c3c78c9c84
vaishnaviid13 <vaishnavideshmukh562@gmail.com>
2026-04-14 00:00:44 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,24 +1,29 @@
-package com.example.assignment2;
+package com.example.constraint_layout;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        backBtn = findViewById(R.id.backBtn);
+
+        // Back to Grid Project
+        backBtn.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(
+                    "com.example.grid_layout",
+                    "com.example.grid_layout.MainActivity"));
+            startActivity(intent);
         });
     }
 }
